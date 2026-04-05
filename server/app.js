61af4932e060181282b601974e8f3e239834377b
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2026-04-05 14:06:25 -0400

    setup - installed packages and added starter server code
    express, nodemon, dotenv, mongodb

---
+++ server/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Jord!')
+})
+
+app.listen(port, () => {
+  console.log(`The server is listening on port ${port}`)
+})
